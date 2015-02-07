22cbf095e8f81d4550cbd5f8b2ab2b27faac7b19
Ivo Pavlik <ivo.pavlik@gmail.com>
2015-02-07 19:02:07 +0100

    Full test script now uses quiet mode to make the output more easily readable.

--- testing/full_test.sh
+++ testing/full_test.sh
@@ -2,7 +2,7 @@
 
 QT4IMAGE="/cygdrive/c/Program Files/Cornell PCG/HDRITools/bin/qt4Image.exe"
 PG3RENDER_BASE_DIR="/cygdrive/c/Users/Ivo/Creativity/Programming/05 PG3 Training/PG3 Training/PG3Render/"
-PG3RENDER64=${PG3RENDER_BASE_DIR}"x64/Release/PG3Render.exe"
+PG3RENDER64=$PG3RENDER_BASE_DIR"x64/Release/PG3Render.exe"
 PG3RENDER=$PG3RENDER64
 
 TESTING_DIR="./testing"
@@ -10,40 +10,72 @@ TESTING_DIR_WIN=".\\testing"
 FULL_TEST_OUTPUT_DIR=$TESTING_DIR"/full_test_output"
 FULL_TEST_OUTPUT_DIR_WIN=$TESTING_DIR_WIN"\\full_test_output"
 
-ITERATIONS_COUNT=4
+ITERATIONS_COUNT=1
+SHORT_OUTPUT=true
+
+TEST_COUNT_TOTAL=0
+TEST_COUNT_SUCCESSFUL=0
+
+# $1 ... algorithm
+# $2 ... scene idx
+run_single_render () {
+    if [ "$SHORT_OUTPUT" = "true" ]; then
+        echo "Rendering: scene $2, $1, $ITERATIONS_COUNT iteration(s)"
+        QUIET_SWITCH="-q"
+    else
+        echo
+        QUIET_SWITCH=""
+    fi
+
+    "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH
+
+    [ "$?" = "0" ] && ((TEST_COUNT_SUCCESSFUL+=1))
+    ((TEST_COUNT_TOTAL+=1))
+}
+
+# dtto
+# $3 ... environment map
+run_single_render_em () {
+    if [ "$SHORT_OUTPUT" = "true" ]; then
+        echo "Rendering: scene $2, em $3, $1, $ITERATIONS_COUNT iteration(s)"
+        QUIET_SWITCH="-q"
+    else
+        echo
+        QUIET_SWITCH=""
+    fi
+
+    "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -em $3 -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH
+
+    [ "$?" = "0" ] && ((TEST_COUNT_SUCCESSFUL+=1))
+    ((TEST_COUNT_TOTAL+=1))
+}
 
 # $1 ... scene idx
-# $2 ... iterations
-# $3 ... environment map (optional)
-run_render () {
+# $2 ... environment map (optional)
+run_rendering_set () {
     echo
-    echo "===================================================================="
+    if [ "$SHORT_OUTPUT" != "true" ]; then
+        echo "===================================================================="
+    fi
 
-    if [ "$3" == "" ]; then
-        echo "Rendering scene $1, $2 iterations"
+    if [ "$2" == "" ]; then
+        if [ "$SHORT_OUTPUT" != "true" ]; then
+            echo "Rendering scene $1"
+        fi
 
-        # TODO: delete old output file(s) - we need the name
-        #rm -f $1.art $1.artraw $1.tiff
-
-        echo
-        "$PG3RENDER" -a dbs  -s $1 -i $2 -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN"
-        echo
-        "$PG3RENDER" -a dlss -s $1 -i $2 -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN"
-        echo
-        "$PG3RENDER" -a dlsa -s $1 -i $2 -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN"
-        echo
-        "$PG3RENDER" -a dmis -s $1 -i $2 -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN"
+        run_single_render "dbs " $1
+        run_single_render dlss $1
+        run_single_render dlsa $1
+        run_single_render dmis $1
     else
-        echo "Rendering scene $1, $2 iterations, em $3"
+        if [ "$SHORT_OUTPUT" != "true" ]; then
+            echo "Rendering scene $1, em $2"
+        fi
 
-        echo
-        "$PG3RENDER" -a dbs  -s $1 -i $2 -e hdr -em $3 -od "$FULL_TEST_OUTPUT_DIR_WIN"
-        echo
-        "$PG3RENDER" -a dlss -s $1 -i $2 -e hdr -em $3 -od "$FULL_TEST_OUTPUT_DIR_WIN"
-        echo
-        "$PG3RENDER" -a dlsa -s $1 -i $2 -e hdr -em $3 -od "$FULL_TEST_OUTPUT_DIR_WIN"
-        echo
-        "$PG3RENDER" -a dmis -s $1 -i $2 -e hdr -em $3 -od "$FULL_TEST_OUTPUT_DIR_WIN"
+        run_single_render_em "dbs " $1 $2
+        run_single_render_em dlss $1 $2
+        run_single_render_em dlsa $1 $2
+        run_single_render_em dmis $1 $2
     fi
 }
 
@@ -59,17 +91,18 @@ rm -f "$FULL_TEST_OUTPUT_DIR"/*.hdr "$FULL_TEST_OUTPUT_DIR"/*.bmp
 scenes_with_em="6 7 8 9 10 11 12 13 15 16 17"
 for scene in `seq 0 17`;
 do
-    echo
     if [[ $scenes_with_em =~ (^| )$scene($| ) ]]; then
         for em in `seq 0 10`;
         do
-            run_render $scene $ITERATIONS_COUNT $em
+            run_rendering_set $scene $em
         done
     else
-        run_render $scene $ITERATIONS_COUNT
+        run_rendering_set $scene
     fi
 done
 
 echo
 echo "The script has finished."
+echo "Success: $TEST_COUNT_SUCCESSFUL out of $TEST_COUNT_TOTAL"
+
 read
