23a64911af49d8f1af9a33823bb787fa534e64a2
Ivo Pavlik <ivo.pavlik@gmail.com>
2015-04-05 23:15:26 +0200

    minors

--- testing/custom_batch.sh
+++ testing/custom_batch.sh
@@ -12,29 +12,29 @@ cd "$PG3RENDER_BASE_DIR"
 #pwd
 #echo
 
-#"$PG3RENDER" -a ptnm -i 70000  -s 0 -od ".\output images" -e hdr
+#"$PG3RENDER" -a ptnm -i 70000  -s 0 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
 #echo
-#"$PG3RENDER" -a ptnm -i 150000 -s 1 -od ".\output images" -e hdr
+#"$PG3RENDER" -a ptnm -i 20000  -s 6 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
 #echo
-#"$PG3RENDER" -a ptnm -i 80000  -s 2 -od ".\output images" -e hdr
-#echo
-#"$PG3RENDER" -a ptnm -i 150000 -s 3 -od ".\output images" -e hdr
-#echo
-#"$PG3RENDER" -a ptnm -i 80000  -s 4 -od ".\output images" -e hdr
-#echo
-#"$PG3RENDER" -a ptnm -i 150000 -s 5 -od ".\output images" -e hdr
-#echo
-#"$PG3RENDER" -a ptnm -i 20000  -s 6 -od ".\output images" -e hdr
-#echo
-#"$PG3RENDER" -a ptnm -i 50000  -s 7 -od ".\output images" -e hdr
-#echo
+"$PG3RENDER" -a ptnm -i 50000  -s 7 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
+echo
+"$PG3RENDER" -a ptnm -i 150000 -s 1 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
+echo
+"$PG3RENDER" -a ptnm -i 80000  -s 2 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
+echo
+"$PG3RENDER" -a ptnm -i 150000 -s 3 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
+echo
+"$PG3RENDER" -a ptnm -i 80000  -s 4 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
+echo
+"$PG3RENDER" -a ptnm -i 150000 -s 5 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
+echo
 
-"$PG3RENDER" -a dbs  -i   1 -s 12 -od ".\output images" -e hdr
-echo
-"$PG3RENDER" -a dbs  -i  10 -s 12 -od ".\output images" -e hdr
-echo
-"$PG3RENDER" -a dbs  -i 100 -s 12 -od ".\output images" -e hdr
-echo
+#"$PG3RENDER" -a dmis  -i 50000  -s 7 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
+#echo
+#"$PG3RENDER" -a dbs   -i 50000  -s 7 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
+#echo
+#"$PG3RENDER" -a dlss  -i 100000 -s 7 -od ".\output images" -e hdr -ot "WholeBrdfSampling"
+#echo
 
 echo
 echo "The script has finished."
