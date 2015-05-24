810b3935a49b38b6da50b7107ab5b60d1563e51e
ivokabel <ivo.pavlik@gmail.com>
2015-05-24 13:18:42 +0200

    Small optimization of Russian roulette for indirect illumination

--- src/pathtracer.hxx
+++ src/pathtracer.hxx
@@ -208,15 +208,17 @@ protected:
                 }
             }
 
+            float reflectanceEstimate = 1.0f;
+            if ((mMaxPathLength == 0) /*&& (aPathLength > 1)*/)
+                reflectanceEstimate = Clamp(mat.GetRRContinuationProb(wol), 0.0f, 1.0f);
+
             // Generate requested amount of samples by sampling the BRDF 
             // for both direct and indirect illumination
             for (uint32_t sampleNum = 0; sampleNum < brdfSamplesCount; sampleNum++)
             {
                 // Russian roulette (based on reflectance of the whole BRDF)
-                float reflectanceEstimate = 1.0f;
                 if ((mMaxPathLength == 0) /*&& (aPathLength > 1)*/)
                 {
-                    reflectanceEstimate = Clamp(mat.GetRRContinuationProb(wol), 0.0f, 1.0f);
                     const float rnd = mRng.GetFloat();
                     if (rnd > reflectanceEstimate)
                         continue;
