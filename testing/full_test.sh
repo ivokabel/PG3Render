9e153c274b9d6f71c7c4dc06ae41e9cd6eb489d5
ivokabel <ivo.pavlik@gmail.com>
2016-02-15 20:14:24 +0100

    Prototype of automatic testing script implemented.

--- testing/full_test.sh
+++ testing/full_test.sh
@@ -4,10 +4,13 @@
 # Settings
 ###################################################################################################
 
-SCENES_WITH_EM="6 7 8 9 10 11 12 13 15 16 17 18 22"
-SCENES=`seq 0 23`   #`seq 23 23`  #
-ITERATIONS_COUNT=32
-SHORT_OUTPUT=false
+SCENES="25"    #`seq 0 37`               #`seq 0 30`
+ENVIRONMENT_MAPS="1 10"         #`seq 0 12`
+ITERATIONS_COUNT=4      #256    #32
+SHORT_OUTPUT=true
+COMPARISON_MODE="compare_to_reference"              # "generate_references", "compare_to_reference"
+
+SCENES_WITH_EM="6 7 8 9 10 12 13 14 15 20 21 22 23 24 25 26 27 28    35 36 37"
 
 TESTING_DIR="./testing"
 TESTING_DIR_WIN=".\\testing"
@@ -17,81 +20,111 @@ FULL_TEST_OUTPUT_DIR_WIN=$TESTING_DIR_WIN"\\full_test_output"
 ###################################################################################################
 
 QT4IMAGE="/cygdrive/c/Program Files/Cornell PCG/HDRITools/bin/qt4Image.exe"
-PG3RENDER_BASE_DIR="/cygdrive/c/Users/Ivo/Creativity/Programming/05 PG3 Training/PG3 Training/PG3Render/"
+
+PG3_TRAINING_DIR="/cygdrive/c/Users/Ivo/Creativity/Programming/05 PG3 Training"
+PG3_TRAINING_DIR_WIN="C:\\Users\\Ivo\\Creativity\\Programming\\05 PG3 Training"
+
+PG3RENDER_BASE_DIR="$PG3_TRAINING_DIR/PG3 Training/PG3Render/"
 PG3RENDER32=$PG3RENDER_BASE_DIR"Win32/Release/PG3Render.exe"
 PG3RENDER64=$PG3RENDER_BASE_DIR"x64/Release/PG3Render.exe"
-PG3RENDER=$PG3RENDER64
+PG3RENDER=$PG3RENDER32 #$PG3RENDER64
+
+#DIFF_TOOL_BASE_DIR="$PG3_TRAINING_DIR/perceptual-diff/Bin/Win32"
+ DIFF_TOOL_BASE_DIR="$PG3_TRAINING_DIR/perceptual-diff/BinCMake/Release"
+PATH="$DIFF_TOOL_BASE_DIR:$PATH"
+#DIFF_TOOL=PerceptualDiff.exe
+ DIFF_TOOL=perceptualdiff.exe
 
 TEST_COUNT_TOTAL=0
 TEST_COUNT_SUCCESSFUL=0
 
 # $1 ... algorithm
 # $2 ... scene idx
+# $3 ... environment map (can be empty)
 run_single_render () {
     if [ "$SHORT_OUTPUT" = "true" ]; then
-        echo "Rendering: scene $2, $1, $ITERATIONS_COUNT iteration(s)"
+        if [ "$3" = "" ]; then
+            printf 'Testing scene %d, %-4s, %d iteration(s)... ' $2 $1 $ITERATIONS_COUNT 
+        else
+            printf 'Testing scene %d, em %d, %-4s, %d iteration(s)... ' $2 $3 $1 $ITERATIONS_COUNT 
+        fi
         QUIET_SWITCH="-q"
     else
         echo
         QUIET_SWITCH=""
     fi
 
-    "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH
-
-    [ "$?" = "0" ] && ((TEST_COUNT_SUCCESSFUL+=1))
-    ((TEST_COUNT_TOTAL+=1))
-}
-
-# $1 ... algorithm
-# $2 ... scene idx
-# $3 ... environment map
-run_single_render_em () {
-    if [ "$SHORT_OUTPUT" = "true" ]; then
-        echo "Rendering: scene $2, em $3, $1, $ITERATIONS_COUNT iteration(s)"
-        QUIET_SWITCH="-q"
+    if [ "$COMPARISON_MODE" = "generate_references" ]; then
+        OUTPUT_TRAIL="-ot Reference"
     else
-        echo
-        QUIET_SWITCH=""
+        OUTPUT_TRAIL="-ot Current"
     fi
 
-    "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -em $3 -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH
+    if [ "$3" = "" ]; then
+        REFERENCE_IMG=`"$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH -ot Reference -opop`
+        RENDERED_IMG=` "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH $OUTPUT_TRAIL -opop`
+                       "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH $OUTPUT_TRAIL
+    else
+        REFERENCE_IMG=`"$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH -ot Reference -em $3 -opop`
+        RENDERED_IMG=` "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH $OUTPUT_TRAIL -em $3 -opop`
+                       "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH $OUTPUT_TRAIL -em $3
+    fi
 
-    [ "$?" = "0" ] && ((TEST_COUNT_SUCCESSFUL+=1))
+    RENDERING_RESULT=$?
+
+    #echo "Rendering result: $RENDERING_RESULT"
+
+    # Compare to reference
+    if [ "$COMPARISON_MODE" = "compare_to_reference" ] && [ $RENDERING_RESULT = 0 ]; then
+        "$DIFF_TOOL" -mode rmse -gamma 1.0 -rmsethreshold 0.60 "$RENDERED_IMG" "$REFERENCE_IMG"
+        DIFF_RESULT=$?
+    fi
+
+    if [ "$RENDERING_RESULT" = "0" ] && [ "$DIFF_RESULT" = "0" ]; then
+        ((TEST_COUNT_SUCCESSFUL+=1))
+        echo "Passed"
+    else
+        echo "           $RENDERED_IMG"
+    fi
     ((TEST_COUNT_TOTAL+=1))
 }
 
 # $1 ... scene idx
 # $2 ... environment map (optional)
 run_rendering_set () {
-    echo
     if [ "$SHORT_OUTPUT" != "true" ]; then
         echo "===================================================================="
     fi
 
-    if [ "$2" == "" ]; then
-        if [ "$SHORT_OUTPUT" != "true" ]; then
-            echo "Rendering scene $1"
-        fi
-
-        run_single_render "dbs " $1
-        run_single_render dlss   $1
-        run_single_render dlsa   $1
-        run_single_render dmis   $1
-        run_single_render "ptn " $1
-        run_single_render "pt  " $1
-    else
-        if [ "$SHORT_OUTPUT" != "true" ]; then
-            echo "Rendering scene $1, em $2"
-        fi
-
-        run_single_render_em "dbs " $1 $2
-        run_single_render_em dlss   $1 $2
-        run_single_render_em dlsa   $1 $2
-        run_single_render_em dmis   $1 $2
-        run_single_render_em "ptn " $1 $2
-        run_single_render_em "pt  " $1 $2
+    if [ "$SHORT_OUTPUT" != "true" ]; then
+        echo "Rendering scene $1"
     fi
+
+    run_single_render "dbs " $1 $2
+    run_single_render "dlss" $1 $2
+    run_single_render "dlsa" $1 $2
+    run_single_render "dmis" $1 $2
+    run_single_render "ptn " $1 $2
+    run_single_render "pt  " $1 $2
+    echo
 }
+
+display_time () {
+    local T=$1
+    local D=$((T/60/60/24))
+    local H=$((T/60/60%24))
+    local M=$((T/60%60))
+    local S=$((T%60))
+    (( $D > 0 )) && printf '%d days ' $D
+    (( $H > 0 )) && printf '%d hours ' $H
+    (( $M > 0 )) && printf '%d minutes ' $M
+    (( $D > 0 || $H > 0 || $M > 0 )) && printf 'and '
+    printf '%d seconds\n' $S
+}
+
+###################################################################################################
+
+START_TIME=`date +%s`
 
 #echo
 #pwd
@@ -99,13 +132,15 @@ cd "$PG3RENDER_BASE_DIR"
 #pwd
 #echo
 
-rm -f "$FULL_TEST_OUTPUT_DIR"/*.hdr "$FULL_TEST_OUTPUT_DIR"/*.bmp
+rm -f "$FULL_TEST_OUTPUT_DIR"/*_Current.hdr "$FULL_TEST_OUTPUT_DIR"/*_Current.bmp
 #read
+
+mkdir -p "$FULL_TEST_OUTPUT_DIR"
 
 for scene in $SCENES;
 do
     if [[ $SCENES_WITH_EM =~ (^| )$scene($| ) ]]; then
-        for em in `seq 0 11`;
+        for em in $ENVIRONMENT_MAPS;
         do
             run_rendering_set $scene $em
         done
@@ -117,5 +152,9 @@ done
 echo
 echo "The script has finished."
 echo "Success: $TEST_COUNT_SUCCESSFUL/$TEST_COUNT_TOTAL"
+
+END_TIME=`date +%s`
+TOTAL_TIME=`expr $END_TIME - $START_TIME`
+echo "Total execution time:" `display_time $TOTAL_TIME`.
 
 read
--- testing/launcher.bat
+++ testing/launcher.bat
@@ -1,7 +1,6 @@
 @echo off
 
-rem C:\cygwin64\bin\bash.exe "/cygdrive/c/Users/Ivo/Creativity/Programming/05 PG3 Training/PG3 Training/PG3Render/testing/hello world.sh"
-rem C:\cygwin64\bin\bash.exe "/cygdrive/c/Users/Ivo/Creativity/Programming/05 PG3 Training/PG3 Training/PG3Render/testing/full_test.sh"
-C:\cygwin64\bin\bash.exe "/cygdrive/c/Users/Ivo/Creativity/Programming/05 PG3 Training/PG3 Training/PG3Render/testing/custom_batch.sh"
+    C:\cygwin64\bin\bash.exe "/cygdrive/c/Users/Ivo/Creativity/Programming/05 PG3 Training/PG3 Training/PG3Render/testing/full_test.sh"
+rem C:\cygwin64\bin\bash.exe "/cygdrive/c/Users/Ivo/Creativity/Programming/05 PG3 Training/PG3 Training/PG3Render/testing/custom_batch.sh"
 rem C:\cygwin64\bin\bash.exe "/cygdrive/c/Users/Ivo/Creativity/Programming/05 PG3 Training/PG3 Training/PG3Render/testing/img_diff.sh"
 rem C:\cygwin64\bin\bash.exe "/cygdrive/c/Users/Ivo/Creativity/Programming/05 PG3 Training/PG3 Training/PG3Render/testing/img_diff_to_file.sh"
