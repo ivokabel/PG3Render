a0e00f09a9364d61334fb07d8ea76d84aada5a01
ivokabel <ivo.pavlik@gmail.com>
2016-03-04 20:08:35 +0100

    Full test script much improved: - Various architectures (32, 64) - "make_references_from_currents"

--- testing/full_test.sh
+++ testing/full_test.sh
@@ -4,11 +4,12 @@
 # Settings
 ###################################################################################################
 
-SCENES=`seq 0 37`                           #`seq 0 30`
-ENVIRONMENT_MAPS="1 10"                     #`seq 0 12`
+SCENES=`seq 0 37`
+ENVIRONMENT_MAPS="1 10"                 #`seq 0 12`
 ITERATIONS_COUNT=4
 SHORT_OUTPUT=true
-COMPARISON_MODE="compare_to_reference"  # "make_references_from_currents" # "generate_references"  # 
+COMPARISON_MODE="generate_references"  #"make_references_from_currents"  #"compare_to_reference"  #
+ARCH_MODES="32 64"
 
 SCENES_WITH_EM="6 7 8 9 10 12 13 14 15 20 21 22 23 24 25 26 27 28    35 36 37"
 
@@ -27,13 +28,10 @@ PG3_TRAINING_DIR_WIN="C:\\Users\\Ivo\\Creativity\\Programming\\05 PG3 Training"
 PG3RENDER_BASE_DIR="$PG3_TRAINING_DIR/PG3 Training/PG3Render/"
 PG3RENDER32=$PG3RENDER_BASE_DIR"Win32/Release/PG3Render.exe"
 PG3RENDER64=$PG3RENDER_BASE_DIR"x64/Release/PG3Render.exe"
-PG3RENDER=$PG3RENDER32 #$PG3RENDER64 #
 
-#DIFF_TOOL_BASE_DIR="$PG3_TRAINING_DIR/perceptual-diff/Bin/Win32"
- DIFF_TOOL_BASE_DIR="$PG3_TRAINING_DIR/perceptual-diff/BinCMake/Release"
+DIFF_TOOL_BASE_DIR="$PG3_TRAINING_DIR/perceptual-diff/BinCMake/Release"
 PATH="$DIFF_TOOL_BASE_DIR:$PATH"
-#DIFF_TOOL=PerceptualDiff.exe
- DIFF_TOOL=perceptualdiff.exe
+DIFF_TOOL=perceptualdiff.exe
 
 TEST_COUNT_TOTAL=0
 TEST_COUNT_SUCCESSFUL=0
@@ -44,9 +42,9 @@ TEST_COUNT_SUCCESSFUL=0
 run_single_render () {
     if [ "$SHORT_OUTPUT" = "true" ]; then
         if [ "$3" = "" ]; then
-            printf 'Testing scene %d, %-4s, %d iteration(s)... ' $2 $1 $ITERATIONS_COUNT 
+            printf '[%s] Testing scene %d, %-4s, %d iteration(s)... ' $arch_mode $2 $1 $ITERATIONS_COUNT 
         else
-            printf 'Testing scene %d, em %2d, %-4s, %d iteration(s)... ' $2 $3 $1 $ITERATIONS_COUNT 
+            printf '[%s] Testing scene %d, em %2d, %-4s, %d iteration(s)... ' $arch_mode $2 $3 $1 $ITERATIONS_COUNT 
         fi
         QUIET_SWITCH="-q"
     else
@@ -62,15 +60,15 @@ run_single_render () {
 
     # Generate image names and render
     if [ "$3" = "" ]; then
-        REFERENCE_IMG=` "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH -ot 1Reference -opop`
-        DIFF_IMG=`      "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH -ot 3Diff -opop`
-        RENDERED_IMG=`  "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH $OUTPUT_TRAIL -opop`
-                        "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH $OUTPUT_TRAIL
+        REFERENCE_IMG=` "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_ARCH_WIN" $QUIET_SWITCH -ot 1Reference -opop`
+        DIFF_IMG=`      "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_ARCH_WIN" $QUIET_SWITCH -ot 3Diff -opop`
+        RENDERED_IMG=`  "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_ARCH_WIN" $QUIET_SWITCH $OUTPUT_TRAIL -opop`
+                        "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_ARCH_WIN" $QUIET_SWITCH $OUTPUT_TRAIL
     else
-        REFERENCE_IMG=` "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH -ot 1Reference -em $3 -opop`
-        DIFF_IMG=`      "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH -ot 3Diff -em $3 -opop`
-        RENDERED_IMG=`  "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH $OUTPUT_TRAIL -em $3 -opop`
-                        "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_WIN" $QUIET_SWITCH $OUTPUT_TRAIL -em $3
+        REFERENCE_IMG=` "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_ARCH_WIN" $QUIET_SWITCH -ot 1Reference -em $3 -opop`
+        DIFF_IMG=`      "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_ARCH_WIN" $QUIET_SWITCH -ot 3Diff -em $3 -opop`
+        RENDERED_IMG=`  "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_ARCH_WIN" $QUIET_SWITCH $OUTPUT_TRAIL -em $3 -opop`
+                        "$PG3RENDER" -a $1 -s $2 -i $ITERATIONS_COUNT -e hdr -od "$FULL_TEST_OUTPUT_DIR_ARCH_WIN" $QUIET_SWITCH $OUTPUT_TRAIL -em $3
     fi
     RENDERING_RESULT=$?
 
@@ -142,41 +140,60 @@ cd "$PG3RENDER_BASE_DIR"
 #pwd
 #echo
 
-mkdir -p "$FULL_TEST_OUTPUT_DIR"
+for arch_mode in $ARCH_MODES;
+do
+    if [ "$arch_mode" = "32" ]; then
+        PG3RENDER=$PG3RENDER32
+    else if [ "$arch_mode" = "64" ]; then
+        PG3RENDER=$PG3RENDER64
+    else
+        echo "Unknown architecture was specified!"
+        exit
+    fi fi
 
-if [ "$COMPARISON_MODE" = "generate_references" ]; then
-    rm -f "$FULL_TEST_OUTPUT_DIR"/*_1Reference.hdr "$FULL_TEST_OUTPUT_DIR"/*_1Reference.bmp
-    rm -f "$FULL_TEST_OUTPUT_DIR"/*_2Current.hdr   "$FULL_TEST_OUTPUT_DIR"/*_2Current.bmp
-    rm -f "$FULL_TEST_OUTPUT_DIR"/*_3Diff.hdr      "$FULL_TEST_OUTPUT_DIR"/*_3Diff.bmp
-else if [ "$COMPARISON_MODE" = "compare_to_reference" ]; then
-    rm -f "$FULL_TEST_OUTPUT_DIR"/*_2Current.hdr "$FULL_TEST_OUTPUT_DIR"/*_2Current.bmp
-    rm -f "$FULL_TEST_OUTPUT_DIR"/*_3Diff.hdr    "$FULL_TEST_OUTPUT_DIR"/*_3Diff.bmp
-#else if [ "$COMPARISON_MODE" = "make_references_from_currents" ]; then
-#    rm -f "$FULL_TEST_OUTPUT_DIR"/*_1Reference.hdr "$FULL_TEST_OUTPUT_DIR"/*_1Reference.bmp
-#    rm -f "$FULL_TEST_OUTPUT_DIR"/*_3Diff.hdr      "$FULL_TEST_OUTPUT_DIR"/*_3Diff.bmp
-#    mv -f "$FULL_TEST_OUTPUT_DIR"/*_2Current.* "$FULL_TEST_OUTPUT_DIR"/*_1Reference.*
-#fi
-fi fi
+    FULL_TEST_OUTPUT_DIR_ARCH="${FULL_TEST_OUTPUT_DIR}/${arch_mode}"
+    FULL_TEST_OUTPUT_DIR_ARCH_WIN="${FULL_TEST_OUTPUT_DIR_WIN}\\${arch_mode}"
 
-if [ "$COMPARISON_MODE" != "make_references_from_currents" ]; then
-    for scene in $SCENES;
-    do
-        if [[ $SCENES_WITH_EM =~ (^| )$scene($| ) ]]; then
-            for em in $ENVIRONMENT_MAPS;
-            do
-                run_rendering_set $scene $em
-            done
-        else
-            run_rendering_set $scene
-        fi
-    done
-
+    echo "============================="
+    echo "Running for architecture: $arch_mode"
+    echo "============================="
     echo
-    echo "The script has finished."
 
-    TEST_COUNT_UNSUCCESSFUL=`expr $TEST_COUNT_TOTAL - $TEST_COUNT_SUCCESSFUL`
-    echo "Successful: $TEST_COUNT_SUCCESSFUL, unsuccessful: $TEST_COUNT_UNSUCCESSFUL"
-fi
+    mkdir -p "$FULL_TEST_OUTPUT_DIR_ARCH"
+
+    if [ "$COMPARISON_MODE" = "generate_references" ]; then
+        rm -f "$FULL_TEST_OUTPUT_DIR_ARCH"/*_1Reference.hdr "$FULL_TEST_OUTPUT_DIR_ARCH"/*_1Reference.bmp
+        rm -f "$FULL_TEST_OUTPUT_DIR_ARCH"/*_2Current.hdr   "$FULL_TEST_OUTPUT_DIR_ARCH"/*_2Current.bmp
+        rm -f "$FULL_TEST_OUTPUT_DIR_ARCH"/*_3Diff.hdr      "$FULL_TEST_OUTPUT_DIR_ARCH"/*_3Diff.bmp
+    else if [ "$COMPARISON_MODE" = "compare_to_reference" ]; then
+        rm -f "$FULL_TEST_OUTPUT_DIR_ARCH"/*_2Current.hdr "$FULL_TEST_OUTPUT_DIR_ARCH"/*_2Current.bmp
+        rm -f "$FULL_TEST_OUTPUT_DIR_ARCH"/*_3Diff.hdr    "$FULL_TEST_OUTPUT_DIR_ARCH"/*_3Diff.bmp
+    else if [ "$COMPARISON_MODE" = "make_references_from_currents" ]; then
+        rm -f "$FULL_TEST_OUTPUT_DIR_ARCH"/*_1Reference.hdr "$FULL_TEST_OUTPUT_DIR_ARCH"/*_1Reference.bmp
+        rm -f "$FULL_TEST_OUTPUT_DIR_ARCH"/*_3Diff.hdr      "$FULL_TEST_OUTPUT_DIR_ARCH"/*_3Diff.bmp
+        rename -v '2Current' '1Reference' "$FULL_TEST_OUTPUT_DIR_ARCH/"*_2Current.*
+    fi fi fi
+
+    if [ "$COMPARISON_MODE" != "make_references_from_currents" ]; then
+        for scene in $SCENES;
+        do
+            if [[ $SCENES_WITH_EM =~ (^| )$scene($| ) ]]; then
+                for em in $ENVIRONMENT_MAPS;
+                do
+                    run_rendering_set $scene $em
+                done
+            else
+                run_rendering_set $scene
+            fi
+        done
+    fi
+done
+
+echo
+echo "The script has finished."
+
+TEST_COUNT_UNSUCCESSFUL=`expr $TEST_COUNT_TOTAL - $TEST_COUNT_SUCCESSFUL`
+echo "Successful: $TEST_COUNT_SUCCESSFUL, unsuccessful: $TEST_COUNT_UNSUCCESSFUL"
 
 END_TIME=`date +%s`
 TOTAL_TIME=`expr $END_TIME - $START_TIME`
