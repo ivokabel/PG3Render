98697ce5b52a28c989f706782d64900db20a441d
ivokabel <ivo.pavlik@gmail.com>
2017-05-08 17:42:29 +0200

    AlignedMalloc: Zero memory & nullptr checking

--- src/memory.hxx
+++ src/memory.hxx
@@ -6,13 +6,20 @@
 
 namespace Memory
 {
-    void* AlignedMalloc(std::size_t aAlignment, std::size_t aSize)
+    void* AlignedMalloc(std::size_t aAlignment, std::size_t aSize, bool aZeroMemory)
     {
         // TODO: Aligned allocation: platform-dependent
         aAlignment; // unused so far
-        return std::malloc(aSize);
+        void * result = std::malloc(aSize);
+        if (result == nullptr)
+            return nullptr;
 
         // TODO: Later C++17 std::aligned_alloc()
+
+        if (aZeroMemory)
+            std::memset(result, 0, aSize);
+
+        return result;
     }
     
     void AlignedFree(void* aData)
