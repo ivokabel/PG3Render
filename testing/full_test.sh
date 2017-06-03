3524ea822e26707aaf2c0d3621928471e4668f0f
ivokabel <ivo.pavlik@gmail.com>
2017-06-03 14:09:28 +0200

    Full test

--- testing/full_test.sh
+++ testing/full_test.sh
@@ -6,6 +6,9 @@
 
 COMPARISON_MODE="compare_to_reference"  #make_references_from_currents"  #"generate_references"  #"
 ITERATIONS_COUNTS="4 16"
+ARCH_MODES="32 64"
+ERROR_THRESHOLD=0.25
+
 SCENES="0 1 2 3 4 5 6 7 8 9   13 14   16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36"
 declare -A SCENE_EM_MAP=(
     ["6"]=" 1    10"
@@ -26,8 +29,6 @@ declare -A SCENE_EM_MAP=(
     ["36"]="1    10"
 )
 SHORT_OUTPUT=true
-ARCH_MODES="32 64"
-ERROR_THRESHOLD=1.0 #4.0
 
 TESTING_DIR="./testing"
 TESTING_DIR_WIN=".\\testing"
