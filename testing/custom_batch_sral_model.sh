05f553a30d6cf8a396368ffa4c642e68032d7bdd
ivokabel <ivo.pavlik@gmail.com>
2018-04-30 15:03:20 +0200

    SRAL gallery: GlossyMediumSmooth

--- testing/custom_batch_sral_model.sh
+++ testing/custom_batch_sral_model.sh
@@ -129,23 +129,23 @@ INNER_LAMB_COLOUR_OT=""
 THICK=1.0
 MEDIUM_BLUE=0.0
 MEDIUM_OT="Orange"
-FILENAME_LIST=()
-for EM in 1 7 10; do
-              render -s 27 -em $EM -a dmis -i $ITERS \
-                     -auxf1 $OUTER_ROUGH -auxf2 $INNER_ROUGH -auxf3 $THICK -auxf4 ${INNER_LAMB_COLOUR} -auxf5 ${MEDIUM_BLUE} \
-                     -ot ${IMG_NAME}_Inner${INNER_ROUGH}${INNER_LAMB_COLOUR_OT}_Thick${THICK}${MEDIUM_OT}_Outer${OUTER_ROUGH}
-    FILENAME=`render -s 27 -em $EM -a dmis -i $ITERS \
-                     -auxf1 $OUTER_ROUGH -auxf2 $INNER_ROUGH -auxf3 $THICK -auxf4 ${INNER_LAMB_COLOUR} -auxf5 ${MEDIUM_BLUE} \
-                     -ot ${IMG_NAME}_Inner${INNER_ROUGH}${INNER_LAMB_COLOUR_OT}_Thick${THICK}${MEDIUM_OT}_Outer${OUTER_ROUGH} \
-                     -opof`
-    FILENAME_LIST+=("$FILENAME")
-done
-stitch_images FILENAME_LIST "${IMG_NAME}_EM${EM}_${ITERS}s.jpg"
+#FILENAME_LIST=()
+#for EM in 1 7 10; do
+#              render -s 27 -em $EM -a dmis -i $ITERS \
+#                     -auxf1 $OUTER_ROUGH -auxf2 $INNER_ROUGH -auxf3 $THICK -auxf4 ${INNER_LAMB_COLOUR} -auxf5 ${MEDIUM_BLUE} \
+#                     -ot ${IMG_NAME}_Inner${INNER_ROUGH}${INNER_LAMB_COLOUR_OT}_Thick${THICK}${MEDIUM_OT}_Outer${OUTER_ROUGH}
+#    FILENAME=`render -s 27 -em $EM -a dmis -i $ITERS \
+#                     -auxf1 $OUTER_ROUGH -auxf2 $INNER_ROUGH -auxf3 $THICK -auxf4 ${INNER_LAMB_COLOUR} -auxf5 ${MEDIUM_BLUE} \
+#                     -ot ${IMG_NAME}_Inner${INNER_ROUGH}${INNER_LAMB_COLOUR_OT}_Thick${THICK}${MEDIUM_OT}_Outer${OUTER_ROUGH} \
+#                     -opof`
+#    FILENAME_LIST+=("$FILENAME")
+#done
+#stitch_images FILENAME_LIST "${IMG_NAME}_EM${EM}_${ITERS}s.jpg"
 
 # Inner Glossy - Outer Smooth
 
 IMG_NAME=${OT_BASE}_GlossyMediumSmooth
-INNER_ROUGH=0.14
+INNER_ROUGH=0.25
 OUTER_ROUGH=0.01
 INNER_LAMB_COLOUR=0
 INNER_LAMB_COLOUR_OT=""
